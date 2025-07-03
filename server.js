b85d6f0b63948183650fff47e06a2044170e6ce3
KEERTHANA K <keerthana.k2022@vitstudent.ac.in>
2025-07-03 08:49:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ io.on('connection', socket => {
 });
 
 server.listen(3000, () => {
-    console.log('✅ Server running at http://localhost:3000');
+    console.log('Server running at http://localhost:3000');
 });
